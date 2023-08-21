55ad05df7f4d2b454c93e734c7af6265c1a38ec8
albralelie10 <122433738+albralelie10@users.noreply.github.com>
2023-08-21 07:39:06 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,13 +13,11 @@ const connectDB=require("./db/connection")
 //   next();
 // });
 
-// app.use(cors({
-//   origin: "https://simple-mern-recetario-frontend.vercel.app",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
-
-app.use(cors())
+app.use(cors({
+origin: "",
+methods: ["GET", "POST", "PUT", "DELETE"],
+credentials: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
