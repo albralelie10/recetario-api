62209fbe4bf7cdddde1af5e1f4db8aec8744c626
albralelie10 <122433738+albralelie10@users.noreply.github.com>
2023-08-21 07:47:36 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const router=require("./routes/route")
 const connectDB=require("./db/connection")
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://recetario-frontend.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
